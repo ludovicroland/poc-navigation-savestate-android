023451f324e8693f08039452c2d7d0e7c04054eb
Ludovic Roland <ludovic.c.roland@gmail.com>
2022-12-02 23:14:33 +0100

    fix issue

--- app/src/main/java/fr/rolandl/poc/instance/ui/dashboard/DashboardAdapter.kt
+++ app/src/main/java/fr/rolandl/poc/instance/ui/dashboard/DashboardAdapter.kt
@@ -1,26 +1,19 @@
 package fr.rolandl.poc.instance.ui.dashboard
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import fr.rolandl.poc.instance.ui.recycler.ItemFragment
 
-class DashboardAdapter(fragment: FragmentManager, lifecycle: Lifecycle) :
+class DashboardAdapter(fragment: FragmentManager, lifecycle: Lifecycle, val List: List<Int>) :
   FragmentStateAdapter(fragment, lifecycle)
 {
 
-  private var list = emptyList<Int>()
-
   override fun getItemCount(): Int =
-    list.size
+    List.size
 
   override fun createFragment(position: Int): Fragment =
     ItemFragment()
 
-  fun updateList(list: List<Int>) {
-    this.list = list
-    notifyDataSetChanged()
-  }
 }
--- app/src/main/java/fr/rolandl/poc/instance/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/fr/rolandl/poc/instance/ui/dashboard/DashboardFragment.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import fr.rolandl.poc.instance.databinding.FragmentDashboardBinding
 import kotlinx.coroutines.Dispatchers
@@ -38,19 +36,17 @@ class DashboardFragment : Fragment()
     _binding = FragmentDashboardBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
-    adapter = DashboardAdapter(childFragmentManager, viewLifecycleOwner.lifecycle)
-    _binding?.viewPager?.adapter = adapter
+    viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+      delay(2000)
 
-    _binding?.let {
-      TabLayoutMediator(it.tabs, it.viewPager) { tab, position ->
-        tab.text = "TAB ${(position + 1)}"
-      }.attach()
+      withContext(Dispatchers.Main) {
+        adapter = DashboardAdapter(childFragmentManager, viewLifecycleOwner.lifecycle, (1..5).toList())
+        _binding?.viewPager?.adapter = adapter
 
-      viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
-        delay(2000)
-
-        withContext(Dispatchers.Main) {
-          adapter.updateList((1..5).toList())
+        _binding?.let {
+          TabLayoutMediator(it.tabs, it.viewPager) { tab, position ->
+            tab.text = "TAB ${(position + 1)}"
+          }.attach()
         }
       }
     }
